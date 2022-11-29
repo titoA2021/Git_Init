71d597250095c14d27fb80fa2b6e1c25da839c12
titozapata <titoalejandro97@gmail.com>
2022-11-29 13:01:06 -0500

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Subiendo un archivo a gitHub desde intelliJ");
+        System.out.println("El primer cambio");
     }
 }
